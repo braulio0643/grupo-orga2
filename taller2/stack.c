29d4fb8816949de2eed83114830eee6e5bc587dd
braulio0643 <93560561+braulio0643@users.noreply.github.com>
2022-08-25 21:50:50 -0300

    ola
    ola

--- taller2/stack.c
+++ taller2/stack.c
@@ -6,7 +6,7 @@
 /* Desapila el último elemento (i.e. lo devuelve y lo elimina de la pila)
  */
 uint64_t pop(stack_t *stack) {
-  uint64_t res = stack->top;
+  uint64_t res = *(stack->esp);
   stack->esp++;
   return res;
 }
@@ -17,13 +17,15 @@ uint64_t top(stack_t *stack) { return *(stack->esp); }
 
 /* Apila el contenido de 'data'
  */
-void push(stack_t *stack, uint64_t data) {}
+void push(stack_t *stack, uint64_t data) {
+  stack->esp++;
+  *(stack->esp)= data;
+}
 
 stack_t *createStack(size_t size) {
   // Completar: Pedir memoria para el stack
-  stack_t *stack = malloc(size);
-
-  stack->_stackMem = stack;
+  stack_t *stack = malloc(sizeof(stack_t));
+  stack->_stackMem = malloc(size);
   stack->ebp = stack->_stackMem + size -
                1; // La base del stack es la dirección numéricamente más grande.
   stack->esp = stack->ebp; // Al comienzo, la base y el tope coinciden
@@ -36,5 +38,7 @@ stack_t *createStack(size_t size) {
 }
 
 void deleteStack(stack_t *stack) {
+  free((stack->_stackMem));
+  free(stack);
   // Completar
 }
