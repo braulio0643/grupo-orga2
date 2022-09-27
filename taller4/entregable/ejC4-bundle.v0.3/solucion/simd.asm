7999a6b5af7b62a096a7eb29f30114b599b20698
Agustin <agustin.azar101@gmail.com>
2022-09-27 18:27:36 -0300

    simd

--- taller4/entregable/ejC4-bundle.v0.3/solucion/simd.asm
+++ taller4/entregable/ejC4-bundle.v0.3/solucion/simd.asm
@@ -18,7 +18,8 @@ shl rcx,1
 shr rcx,6
 push rsi
 ;filtro->buffer[filtro->length - 1]..
-mov r8, rdi + BUFFER_OFFSET
+mov r8, rdi
+add r8, BUFFER_OFFSET
 add r8, [rdi + LENGTH_OFFSET]
 dec r8
 memcpytrucho:
@@ -38,11 +39,12 @@ xor r10,r10
     mov rcx, r9
     dec r9
     add r9, r10
-    add r9, rdi + BUFFER_OFFSET ; posicion a leer del buffer
+    add r9, rdi
+    add r9,BUFFER_OFFSET ; posicion a leer del buffer
         .innerloop:
             ;acc += (int32_t)(*coeff_p++) * (int32_t)(*(in_p--))
-            movups xmm1, qword [r9] ; cargo 16 bytes buffer en xmm1
-            pmaddwd xmm1, qword [r8] ; multiplicamos los coefs con el buffer
+            movups xmm1, [r9] ; cargo 16 bytes buffer en xmm1
+            pmaddwd xmm1, [r8] ; multiplicamos los coefs con el buffer
             paddd xmm0,xmm1
             sub rcx, 16
             add r9, 16
@@ -51,11 +53,11 @@ xor r10,r10
         jg .innerloop
     phaddd xmm0, xmm0
     phaddd xmm0, xmm0
-    cmp xmm0, 0x3fffffff
+    cmp qword xmm0, 0x3fffffff
     jle .no_saturar_mayor
     mov xmm0,0x3fffffff
     .no_saturar_mayor:
-    cmp xmm0, -0x40000000
+    cmp qword xmm0, -0x40000000
     jge .fin
     mov xmm0,-0x40000000
     .fin:
@@ -68,7 +70,8 @@ xor r10,r10
     cmp r10,rdx
     jl .loopexterno
 
-mov r8, rdi + BUFFER_OFFSET
+mov r8, rdi
+add r8, BUFFER_OFFSET
 mov r9,r8
 add r9, rdx
 dec r9
