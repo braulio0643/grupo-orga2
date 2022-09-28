ed03d6f7c828ed55e91f72b57d21ce75451693f7
Agustin <agustin.azar101@gmail.com>
2022-09-28 19:42:37 -0300

    fix ej4

--- taller4/entregable/ejC4-bundle.v0.3/solucion/filtros.c
+++ taller4/entregable/ejC4-bundle.v0.3/solucion/filtros.c
@@ -3,47 +3,39 @@
 
 #include "filtros.h"
 
-void fir_init(FIR_t *filtro)
-{
-    memset(filtro->buffer, 0, sizeof(filtro->buffer));
+void fir_init(FIR_t *filtro) {
+  memset(filtro->buffer, 0, sizeof(filtro->buffer));
 }
 
-size_t fir_filter(FIR_t *filtro, int16_t *in, unsigned length, int16_t *out)
-{
-    int32_t acc;   // acumulador para MACs
-    int16_t *in_p; // puntero a las muestras de entrada
+size_t fir_filter(FIR_t *filtro, int16_t *in, unsigned length, int16_t *out) {
+  int32_t acc;   // acumulador para MACs
+  int16_t *in_p; // puntero a las muestras de entrada
 
-    memcpy(&filtro->buffer[filtro->length - 1], in, length * sizeof(int16_t));
-    
+  memcpy(&filtro->buffer[filtro->length - 1], in, length * sizeof(int16_t));
 
-    // aplicar el filtro
-    int16_t *coeff_p;
+  // aplicar el filtro
+  int16_t *coeff_p;
 
-    for (unsigned n = 0; n < length; n++)
-    {
-        coeff_p = filtro->coefs;
-        in_p = &filtro->buffer[filtro->length - 1 + n];
-        acc = 0;
-        for (int k = 0; k < filtro->length; k++)
-        {
-            acc += (int32_t)(*coeff_p++) * (int32_t)(*(in_p--));
-        }
-
-        // saturar el resultado
-        if (acc > 0x3fffffff)
-        {
-            acc = 0x3fffffff;
-        }
-        else if (acc < -0x40000000)
-        {
-            acc = -0x40000000;
-        }
-        // convertir de Q30 a Q15
-        out[n] = (int16_t)(acc >> 15);
+  for (unsigned n = 0; n < length; n++) {
+    coeff_p = filtro->coefs;
+    in_p = &filtro->buffer[filtro->length - 1 + n];
+    acc = 0;
+    for (int k = 0; k < filtro->length; k++) {
+      acc += (int32_t)(*coeff_p++) * (int32_t)(*(in_p--));
     }
 
-    memmove(&filtro->buffer[0], &filtro->buffer[length], (filtro->length - 1) * sizeof(int16_t));
+    // saturar el resultado
+    if (acc > 0x3fffffff) {
+      acc = 0x3fffffff;
+    } else if (acc < -0x40000000) {
+      acc = -0x40000000;
+    }
+    // convertir de Q30 a Q15
+    out[n] = (int16_t)(acc >> 15);
+  }
 
-    return length;
+  memmove(&filtro->buffer[0], &filtro->buffer[length],
+          (filtro->length - 1) * sizeof(int16_t));
+
+  return length;
 }
-
--- taller4/entregable/ejC4-bundle.v0.3/solucion/simd.asm
+++ taller4/entregable/ejC4-bundle.v0.3/solucion/simd.asm
@@ -3,9 +3,8 @@ extern fir_filter
 extern memcpy
 extern memmove
 %define COEFS_OFFSET 0
-%define LENGTH_OFFSET 4
-%define BUFFER_OFFSET 8
-%define FIR_T_SIZE 12
+%define LENGTH_OFFSET 8
+%define BUFFER_OFFSET 12
 ; size_t fir_filter_simd(FIR_t*filtro[rdi], int16_t *in[rsi], unsigned length[edx], int16_t *out[rcx]);
 fir_filter_simd:
 
@@ -18,13 +17,18 @@ push rsi ;alineada
 push rdx ;desalineada
 push rcx ;alineada
 
-mov r8, rdi
-add r8, BUFFER_OFFSET
-add r8, [rdi + LENGTH_OFFSET]
-dec r8; filtro->buffer[filtro->length - 1]
-mov rdi, r8
+;memcpy(&filtro->buffer[filtro->length - 1], in, length * sizeof(int16_t));
+mov r8, rdi                     ; *filtro
+add r8, BUFFER_OFFSET           ; *filtro->buffer
+mov r9, rdi                     ; *filtro 
+add r9, LENGTH_OFFSET           ; *filtro->length
+mov r9, [r9]                    ; filtro->length
+dec r9                          ; filtro->length - 1
+sal r9, 1                       ; 2*(filtro->length - 1) // offset length - 1
+add r8, r9                      ; *filtro->buffer[length - 1]
+mov rdi, r8                     ; *filtro->buffer[length - 1]
+sal edx, 1                      ; length * sizeof(int16_t)
 
-sal rdx, 1
 call memcpy WRT ..plt
 
 pop rcx
@@ -36,12 +40,13 @@ xor r10,r10
 
 .loopexterno:
     ;rdi: estructura. rdx: longitud del audio. r8: puntero a coeficiente. r9: (in_p) posicion a leer del buffer
-    pxor xmm0,xmm0 ;acumulador
-    mov r8, [rdi + COEFS_OFFSET] ;coef
-    mov r9, [rdi+LENGTH_OFFSET]; long
-    mov rcx, r9 ; long
+    pxor xmm0,xmm0                 ; acumulador
+    mov r8, [rdi + COEFS_OFFSET]   ; coef
+    mov r9, [rdi+LENGTH_OFFSET]    ; long
+    mov r11, r9 ; long
     add r9, r10 ; length + n
-    dec r9 ; length + n - 1 
+    sub r9, 16 ; length + n - 1 
+    sal r9, 1 ; offset del item [length + n - 1]
     add r9, rdi
     add r9,BUFFER_OFFSET ; filtro->buffer[filtro->length - 1 + n]
         .innerloop:
@@ -49,10 +54,10 @@ xor r10,r10
             movups xmm1, [r9] ; cargo 16 bytes buffer en xmm1
             pmaddwd xmm1, [r8] ; multiplicamos los coefs con el buffer
             paddd xmm0,xmm1
-            sub rcx, 16
+            sub r11, 8
             sub r9, 16
             add r8, 16
-            cmp rcx, 0
+            cmp r11, 0
         jg .innerloop
     phaddd xmm0, xmm0
     phaddd xmm0, xmm0
@@ -66,9 +71,9 @@ xor r10,r10
     mov rsi, -0x40000000
     .fin:
     sar rsi, 15
-    mov rax, r11
-    add rax, r10
-    mov  [r11], rsi
+    mov word [rcx], si
+    inc rcx
+    inc rcx
     inc r10
     cmp r10, rdx
     jl .loopexterno
@@ -76,20 +81,21 @@ xor r10,r10
 
 
 
-mov r8, rdi
-add r8, BUFFER_OFFSET ; buffer
+;; memmove(&filtro->buffer[0], &filtro->buffer[length], (filtro->length - 1) * sizeof(int16_t));
+mov r8, rdi                     ; *filtro
+add r8, BUFFER_OFFSET           ; *filtro->buffer
 xor r9,r9
-add r9d, edx ; buffer[length]
-mov r9, r8
-mov r10, rdi
-add r10, BUFFER_OFFSET
-add r10, [rdi + LENGTH_OFFSET]
-dec r10; filtro->buffer[filtro->length - 1]
-sal r10, 1
+add r9d, edx                    ; length
+sal r9d, 1                      ; length offset
+add r9, r8                      ; *filtro->buffer[length]
+mov r10, [rdi + LENGTH_OFFSET]  ; filtro.length
+dec r10                         ; filtro.length - 1
+sal r10,1                       ; (filtro.length - 1) * sizeof(int16_t)
 push rdx; desalineada
-mov rdi, r8
-mov rcx, r9
-mov rdx, r10
+
+mov rdi, r8                     ; *filtro->buffer
+mov rsi, r9                     ; *filtro->buffer[length]
+mov rdx, r10                    ; (filtro.length - 1) * sizeof(int16_t)
 
 call memmove WRT ..plt
 
